4020c26b70d52624bc56c9074db1c8e3af0d3229
gabrieelgomez <gagg1707@gmail.com>
2020-04-27 19:48:14 -0400

    change domain base

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-// export const BASE_DOMAIN = 'http://192.168.0.139:4000/';
+// export const BASE_DOMAIN = 'https://api-movie-company.herokuapp.com/';
+export const BASE_DOMAIN = 'https://api-movie-company.herokuapp.com'
 export const DEVELOPMENT_DOMAIN = 'http://localhost:3000';
-export const BASE_DOMAIN = 'http://192.168.0.139:4000'
